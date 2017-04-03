4176e17c377e49ed2f6204bee60db7d015d72242
unknown <L6000867@PC0HYAR0.insags.com>
2017-04-03 17:27:11 +0200

    Creación de service

---
+++ app/services/restaurante.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from "angular2/core";
+import {Http, Response} from "angular2/http";
+import "rxjs/add/operator/map";
+import {Observable} from "rxjs/Observable";
+import {Restaurante} from "../model/restaurante";
+
+@Injectable()
+export class RestauranteService{
+    constructor(private _http: Http){}
+
+    getRestaurantes(){
+      return this._http.get("http://localhost/slim/restaurantes-api.php/restaurantes-list").map(res => res.json);
+    }
+}
