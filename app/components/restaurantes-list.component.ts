48b4e6b63d13133b2d5fbbf3b9685a2379f8883c
L6000867 <L6000867>
2017-04-03 16:41:43 +0200

    "Lesson 43"

--- app/components/restaurantes-list.component.ts
+++ app/components/restaurantes-list.component.ts
@@ -1,12 +1,19 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
 import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
+import {RestauranteService} from "../services/restaurante.service"
 
 @Component({
   selector: "restaurantes-list",
-  templateUrl: "app/view/restaurantes-list.html"
+  templateUrl: "app/view/restaurantes-list.html",
+  providers: [RestauranteService]
 })
 
-export class RestaurantesListComponent{
+export class RestaurantesListComponent implements OnInit{
   public titulo:string ="Listado de restaurantes:";
 
+  constructor(private _restauranteService: RestauranteService){}
+
+  ngOnInit(){
+    console.log("restaurantes-list component cargado");
+  }
 }
