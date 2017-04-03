f6c20ae24c33b501fcdfdd07e4666fda9d94e2f9
L6000867 <L6000867>
2017-04-03 16:00:08 +0200

    Entorno preparado

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "angular2/core";
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
+import {RestaurantesListComponent} from "./components/restaurantes-list.component";
+
+
+@Component({
+    selector: "mi-app",
+    templateUrl:"app/view/home.html",
+    directives:[RestaurantesListComponent, ROUTER_DIRECTIVES],
+  })
+
+
+export class AppComponent{
+  public titulo:string = "Restaurantes";
+}
---
+++ app/components/restaurantes-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "angular2/core";
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
+
+@Component({
+  selector: "restaurantes-list",
+  templateUrl: "app/view/restaurantes-list.html"
+})
+
+export class RestaurantesListComponent{
+  public titulo:string ="Listado de restaurantes:";
+
+}
